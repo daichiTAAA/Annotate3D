706b9510468f7f4fc601d7924ec6b076931307fc
DaiichiroNoda <69710300+donachii@users.noreply.github.com>
2023-12-17 23:46:00 +0900

    change to run each server

--- src/mysite/mysite/urls.py
+++ src/mysite/mysite/urls.py
@@ -18,8 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("demo/django_plotly_dash/", include("django_plotly_dash.urls")),
-    path("demo/", include("demo.urls")),
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
 ]
